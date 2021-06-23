f8045d65f120ca94f4375214cbf57861dbda8b61
essbee808 <sheena.myb@gmail.com>
2021-06-22 21:20:03 -0700

    modify routes

---
+++ app/controllers/artist_controller.rb
@@ -0,0 +1,24 @@
+class ArtistController < ApplicationController
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name, :bio)
+  end
+end
