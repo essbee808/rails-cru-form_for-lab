1e330b71e86ef9314f85643955153145ab8f6a5f
essbee808 <sheena.myb@gmail.com>
2021-06-22 22:26:43 -0700

    create songs index view

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   resources :genres, only: [:new, :create, :edit, :show]
   patch 'genres/:id', to: 'genres#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :songs, only: [:new, :create, :edit, :show, :index]
+  patch 'songs/:id', to: 'songs#update'
+  
 end
