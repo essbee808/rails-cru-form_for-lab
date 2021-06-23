aa3ed429819f0c5a8abe466657ed7a441ca51560
essbee808 <sheena.myb@gmail.com>
2021-06-22 21:34:03 -0700

    modify update method in artists controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,16 +4,24 @@ class ArtistsController < ApplicationController
   end
 
   def create
-
+    @artist = Artist.create(artist_params)
+    @artist.save
+    redirect_to artist_path(@artist)
   end
 
   def edit
+    @artist = Artist.find(params[:id])
   end
 
   def update
+    #raise params.inspect
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params)
+    redirect_to artist_path(@artist)
   end
 
   def show
+    @artist = Artist.find(params[:id])
   end
 
   private
