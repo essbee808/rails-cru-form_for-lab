92c1010cf008c213a404da7dd78784bf055497cb
essbee808 <sheena.myb@gmail.com>
2021-06-22 21:23:59 -0700

    add form to artists#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :artist, only: [:new, :create, :edit, :show]
+  resources :artists, only: [:new, :create, :edit, :show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
